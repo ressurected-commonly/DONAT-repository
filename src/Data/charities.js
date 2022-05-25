779130c12deff95a8da240e51e6af6a3e40b3b31
restu <resetuaji6661@gmail.com>
2022-05-25 17:16:19 +0700

    Convert HTML to svelte components

---
+++ src/Data/charities.js
@@ -0,0 +1,18 @@
+module.export = {
+    charities: [
+        {
+            id:1,
+            title: 'projek penggalangan dana',
+            category: 'Money',
+            thumbnail:'https://live.staticflickr.com/65535/52091936051_5371b3da0a_b.jpg',
+            pledged: 0,
+            target: 100000,
+            date_end: new Date('2 June 2022'),
+            profile_photo:'https://live.staticflickr.com/65535/52093636718_45b3b77cee_c.jpg',
+            profile_name:'hamba ALLAH',
+            no_pledges: 0,
+
+        }
+
+    ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+	import DONAT from "../components/DONASI.svelte";
+	import Header from '../components/Header.svelte';
+	import Welcome from '../components/Welcome.svelte';
+	import Promo from '../components/Promo.svelte';
+	import Footer from '../components/Footer.svelte';
+	import {charities} from "../Data/charities.js";
+	let title = "DONASI";
+	
+</script>
+<Header />
+<Welcome />
+<DONAT {charities} />
+<Footer />
